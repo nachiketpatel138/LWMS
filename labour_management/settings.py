19ab72cd0c505c81ef8ad023a7f8160ab0cbf19b
Nachiket <nachiket1386@gmail.com>
2025-09-18 14:24:19 +0530

    Fix static files configuration for Render

--- labour_management/settings.py
+++ labour_management/settings.py
@@ -84,7 +84,7 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
